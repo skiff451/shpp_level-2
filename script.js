ae24c5ffc7446cd56cb9266630eab3093f2cd5e0
Serghey <samsomskiff451@gmail.com>
2020-12-16 22:58:14 +0200

    remake into one folder

---
+++ script.js
@@ -0,0 +1,74 @@
+"use strict";
+
+// first and second
+const buttons = document.querySelectorAll("button"),
+    rectangle = document.querySelector('.rect');
+buttons.forEach(i => {
+    i.addEventListener('click', event => {
+        console.log(event.target.id);
+
+        if (event.target.id == "one") {
+
+            rectangle.style.display = "none";
+
+        } else if (event.target.id == "two") {
+
+            rectangle.remove();
+
+        } else if (event.target.id == "three") {
+
+            rectangle.className = "hidden";
+
+        }
+
+        else if (event.target.id == "forth") {
+
+            rectangle.classList.toggle("hidden");
+
+        }
+    })
+});
+
+// third
+
+const button = document.getElementById('btn'),
+    rectangles = document.querySelectorAll('.sqr');
+
+button.addEventListener('click', () => {
+    rectangles.forEach(i => {
+        i.classList.toggle("hidden");
+    })
+})
+
+//forth
+
+const hideButton = document.getElementById('hide-btn'),
+    input = document.getElementById('main-input');
+
+
+
+hideButton.addEventListener('click', () => {
+    const inputValue = input.value,
+        elements = document.querySelectorAll(inputValue);
+    elements.forEach(element => {
+        element.classList.toggle('hidden');
+    })
+
+})
+
+//fifth
+
+const yellowSqr = document.querySelector('.yellow-sqr');
+
+yellowSqr.addEventListener('click', hello);
+
+function hello() {
+    alert('Hello');
+    yellowSqr.removeEventListener('click', hello);
+    yellowSqr.addEventListener('click', remove);
+}
+
+function remove() {
+    yellowSqr.className = 'hidden';
+}
+
